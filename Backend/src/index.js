86bb22cb57714f243d54c5efca52dd1553a72fbb
darshitjani300 <janidarshit300@gmail.com>
2024-04-01 12:11:07 +0530

    Database connection with app initialization.

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,1 +1,21 @@
-console.log("Hello world");
+import app from "./app.js";
+import connectDb from "./db/db.js";
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: "../.env",
+});
+
+connectDb()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("Error in db connection inside then ", error);
+      process.exit(1);
+    });
+    app.listen(process.env.PORT, () => {
+      console.log("Server is running on Port", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log("Connection Failed !! Index connectDb ", error);
+  });
