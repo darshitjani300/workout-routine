e3678b05ffaebb15622d9a412093c4974e855f10
darshit <janidarshit300@gmail.com>
2024-04-08 01:09:52 +0530

    Fixed App bug

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import  workoutRouter  from "./routes/workout.routes.js";
+import workoutRouter from "./routes/workout.routes.js";
 
 const app = express();
 
